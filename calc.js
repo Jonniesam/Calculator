f813340697f01039d43a96443ab81e28ec47c5ab
Jonniesam <117244803+Jonniesam@users.noreply.github.com>
2022-12-09 14:41:41 -0800

    Refactored code, changed individual buttons to event buttons, seperated digits from operations.

--- calc.js
+++ calc.js
@@ -1,154 +1,16 @@
+const clear_button = document.getElementById('clear')
+const delete_button = document.getElementById('delete')
+const equals_button = document.getElementById('equals')
+const dot_button = document.getElementById('dot')
+const display = document.getElementById('display')
+const output = document.getElementById('output')
+const digits = document.querySelectorAll('#digits')
+const oper = document.querySelectorAll('#oper')
+
+
 const operators = [];
 const array1 = [];
 const array2 = [];
-
-const clear_button = document.getElementById('clear')
-const delete_button = document.getElementById('delete')
-const divide_button = document.getElementById('divide')
-const mutliply_button = document.getElementById('mutliply')
-const subtract_button = document.getElementById('subtract')
-const add_button = document.getElementById('add')
-const equals_button = document.getElementById('equals')
-const dot_button = document.getElementById('dot')
-const zero_button = document.getElementById('0')
-const one_button = document.getElementById('1')
-const two_button = document.getElementById('2')
-const three_button = document.getElementById('3')
-const four_button = document.getElementById('4')
-const five_button = document.getElementById('5')
-const six_button = document.getElementById('6')
-const seven_button = document.getElementById('7')
-const eight_button = document.getElementById('8')
-const nine_button = document.getElementById('9')
-const display = document.getElementById('display')
-const output = document.getElementById('output')
-
-clear_button.addEventListener('click', function () {
-    array1.length = 0;
-    array2.length = 0;
-    operators.length = 0;
-    display.innerText = 0;
-    output.innerText = '';
-})
-
-
-zero_button.addEventListener('click', function () {
-    let val = zero_button.innerHTML;
-    storeVal(val);
-    })
-nine_button.addEventListener('click', function () {
-    let val = nine_button.innerHTML;
-    storeVal(val);
-    })
-eight_button.addEventListener('click', function () {
-    let val = eight_button.innerHTML;
-    storeVal(val);
-    })
-
-seven_button.addEventListener('click', function () {
-    let val = seven_button.innerHTML;
-    storeVal(val);
-})
-
-six_button.addEventListener('click', function () {
-    let val = six_button.innerHTML;
-    storeVal(val);
-})
-
-five_button.addEventListener('click', function () {
-    let val = five_button.innerHTML;
-    storeVal(val);
-})
-
-four_button.addEventListener('click', function () {
-    let val = four_button.innerHTML;
-    storeVal(val);
-})
-
-three_button.addEventListener('click', function () {
-    let val = three_button.innerHTML;
-    storeVal(val);
-})
-
-two_button.addEventListener('click', function () {
-    let val = two_button.innerHTML;
-    storeVal(val);
-})
-
-one_button.addEventListener('click', function () {
-    let val = one_button.innerHTML;
-    storeVal(val);
-})
-
-
-add_button.addEventListener('click', function () {
-    let val = add_button.innerHTML;
-    console.log(val);
-    if (array1.length >=1 && array2.length >=1){
-        let operation = getOp(operators);
-        storeVal(val);
-        let results = operate(getNum(array1), operation, getNum(array2));
-    display.innerHTML = results;
-    output.innerHTML = `${results} ${operators.at(-1)}`;
-    array1.length= 0;
-    array2.length = 0;
-    array1.push(results);
-    } else {
-        storeVal(val);
-    }
-    })
-
-subtract_button.addEventListener('click', function () {
-    let val = subtract_button.innerHTML;
-    console.log(val);
-    if (array1.length >=1 && array2.length >=1){
-        let operation = getOp(operators);
-        storeVal(val);
-        let results = operate(getNum(array1), operation, getNum(array2));
-    display.innerHTML = results;
-    output.innerHTML = `${results} ${operators.at(-1)}`;
-    array1.length= 0;
-    array2.length = 0;
-    array1.push(results);
-    } else {
-        storeVal(val);
-    }
-    })
-
-mutliply_button.addEventListener('click', function () {
-    let val = mutliply_button.innerHTML;
-    console.log(val);
-    if (array1.length >=1 && array2.length >=1){
-        let operation = getOp(operators);
-        storeVal(val);
-        let results = operate(getNum(array1), operation, getNum(array2));
-    display.innerHTML = results;
-    output.innerHTML = `${results} ${operators.at(-1)}`;
-    array1.length= 0;
-    array2.length = 0;
-    array1.push(results);
-    } else {
-        storeVal(val);
-    }
-    })
-
-divide_button.addEventListener('click', function () {
-    let val = divide_button.innerHTML;
-    console.log(val);
-    if (array1.length >=1 && array2.length >=1){
-        let operation = getOp(operators);
-        storeVal(val);
-        let results = operate(getNum(array1), operation, getNum(array2));
-    display.innerHTML = results;
-    output.innerHTML = `${results} ${operators.at(-1)}`;
-    array1.length= 0;
-    array2.length = 0;
-    array1.push(results);
-    } else {
-        storeVal(val);
-    }
-    })
-
 
 
 function getNum(arr) {
@@ -157,41 +19,37 @@ function getNum(arr) {
     return num;
     }
 
-    function getOp(op) {
-        let last = op.at(-1);
-        return last.toString();
+function getOp(op) {
+    let last = op.at(-1);
+    return last.toString();
     }
 
 
-function storeVal(el) {
-    let amount = el;
+function storeVal(e) {
     let check = operators.length >= 1 ? true : false;
-if (el === '+' || el === '-' || el === 'x' || el === '/') {
-    operators.push(amount);
-    console.log(operators);
+if (e === '+' || e === '-' || e === 'x' || e === '/') {
+    operators.push(e);
         return operators;
 }else if (check === false){
-    array1.push(amount);
+    array1.push(e);
     let firstSet = getNum(array1);
     display.innerHTML= firstSet;
-    console.log (firstSet);
 } else if (check === true) {
-    array2.push(amount);
+    array2.push(e);
     let secondSet = getNum(array2);
     display.innerHTML= secondSet;
-    console.log (secondSet);
 };
-
 }
+
 
 equals_button.addEventListener('click', function () {
     let operation = getOp(operators);
-    let results = operate(getNum(array1), operation.toString(), getNum(array2));
+    let results = operate(getNum(array1), operation, getNum(array2));
     display.innerHTML = results;
     output.innerHTML = `${results} ${operators.at(-1)}`;
     array1.length= 0;
-    array1.push(results);
     array2.length = 0;
+    array1.push(results);
 });
 
 delete_button.addEventListener('click', function() {
@@ -204,14 +62,38 @@ delete_button.addEventListener('click', function() {
     }
 });
 
-function equate() {
-        let results = operate(getNum(array1), operation, getNum(array2));
-    display.innerHTML = results;
-    output.innerHTML = `${results} ${operators.at(-1)}`;
-    array1.length= 0;
+clear_button.addEventListener('click', function () {
+    array1.length = 0;
     array2.length = 0;
-    array1.push(results);
-}
+    operators.length = 0;
+    display.innerText = 0;
+    output.innerText = '';
+})
+
+digits.forEach(digit => {
+    digit.addEventListener('click', function (e) {
+    let val = e.target.innerHTML;
+    storeVal(val);
+})
+})
+
+oper.forEach(ope => {
+    ope.addEventListener('click', function(e) {
+        let val = e.target.innerHTML;
+    if(array1.length >= 1 && array2.length >= 1){
+        let operation = getOp(operators);
+        storeVal(val);
+        let results = operate(getNum(array1), operation, getNum(array2));
+        display.innerHTML = results;
+        output.innerHTML = `${results} ${operators.at(-1)}`;
+        array1.length = 0;
+        array2.length = 0;
+        array1.push(results);
+    } else {
+        storeVal(val);
+    }
+    })
+})
 
 
 function sum (a, b) {
@@ -230,8 +112,12 @@ function multi (a, b) {
 }
 
 function divide (a, b) {
+    if(b !==0){
     let num = a / b;
     return num;
+} else {
+    return "58008"
+}
 }
 
 function operate (num1, operation, num2){
@@ -247,5 +133,3 @@ function operate (num1, operation, num2){
     return 'error';
 }
 }
-
-operate(6, 'divide', 2);
